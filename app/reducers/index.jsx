c733fca680ecf6b1641e4fbb90fc38158919f1e3
Danielle YoungSmith <dyoungsmith@gmail.com>
2017-01-16 15:59:18 -0500

    code review comments

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
+  // OB/DYS: can also use import blah from './blah'
   auth: require('./auth').default,
   products: require('./products').default,
   singleProduct: require('./singleProduct').default,
