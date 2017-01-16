4f7a69a0f4602a46ecc06319a04dd0abe7dba808
BuildTools <dennisdeng2002@yahoo.com>
2017-01-16 14:44:34 -0500

    Fixed cart reducer

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
   auth: require('./auth').default,
+  cart: require('./cart').default,
   products: require('./products').default,
   singleProduct: require('./singleProduct').default,
   categories: require('./categories').default
