665c3f0db4d91f73d042b48d270bde6e238ce4e9
Amrom Steinmetz <amroms@gmail.com>
2017-01-17 11:44:11 -0500

    deleted unnecessary class method

--- db/models/orderProducts.js
+++ db/models/orderProducts.js
@@ -6,21 +6,6 @@ const db = require('APP/db');
 const OrderProducts = db.define('order_products', {
   price: Sequelize.INTEGER,
   quantity: Sequelize.INTEGER
-}, {
-  classMethods: {
-    calculateTotal: function(orderId) {
-      return new Promise((resolve, reject) => {
-        this.findAll({
-          where: {
-            order_id: orderId
-          }
-        })
-        .then(products => products.reduce((total, product) => total + product.price, 0))
-        .then(total => resolve(total))
-        .catch(reject);
-      });
-    }
-  }
 });
 
 module.exports = OrderProducts;
