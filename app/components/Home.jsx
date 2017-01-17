585c6a7196496d9bdb892cb1c2550e602d17f6ca
Amrom Steinmetz <amroms@gmail.com>
2017-01-17 11:39:33 -0500

    move place order logic to model

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import store from '../store';
+import {testOrderRoute} from '../reducers/cart';
 
 export default () => (
-  <h1 className="text-center">
-    Welcome to Grace Potter!
-  </h1>
+  <div>
+    <h1 className="text-center">
+      Welcome to Grace Potter!
+    </h1>
+    <div className="text-center">
+      <button type="button" className="btn btn-default" onClick={() => store.dispatch(testOrderRoute())}>
+        Test Order Route
+      </button>
+    </div>
+  </div>
 );
--- db/models/order.js
+++ db/models/order.js
@@ -2,6 +2,8 @@
 
 const Sequelize = require('sequelize');
 const db = require('APP/db');
+const Product = require('./product');
+const OrderProducts = require('./orderProducts');
 
 const Order = db.define('order', {
   address: {
@@ -17,6 +19,35 @@ const Order = db.define('order', {
   },
   totalPrice: Sequelize.INTEGER
 }, {
+  classMethods: {
+    placeOrder: function(address, email, items) {
+      
+      function getItemPriceAndSetQuantity(item) {
+        return Product.findById(item.id)
+        .then(product => product.price)
+        .then(price => ({product_id: item.id, quantity: item.quantity, price}));
+      }
+
+      function caluculateTotalPrice(items) {
+        return items.reduce((total, item) => total + item.price * item.quantity, 0);
+      }
+
+      return db.Promise.map(items, item => getItemPriceAndSetQuantity(item))
+      .then(itemsWithPrice => ({items: itemsWithPrice, totalPrice: caluculateTotalPrice(itemsWithPrice)}))
+      .then(orderInfo => this.create({
+        address,
+        email,
+        items: orderInfo.items,
+        totalPrice: orderInfo.totalPrice
+      }, {
+        include: [{
+          model: OrderProducts,
+          as: 'items'
+        }]
+      }))
+      .then(order => order);
+    }
+  },
   instanceMethods: {
     calculateTotal: function(products) {
       this.totalPrice = products.reduce((total, product) => total + b.price, 0);
